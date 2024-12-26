b998eb5fc69ebc91c5a503281a96cda303dbedec
TheAndrey777 <andreyvasilchenko777@mail.ru>
2024-12-26 19:33:17 +0300

    fix(backend): cors

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express, { Application } from "express";
+import cors from "cors";
+import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import { errorHandler } from "./middlewares/error.middleware";
@@ -7,7 +9,19 @@ import "./strategies/jwt.strategy";
 import { recipeRouter } from "./routes/recipe.router";
 import { userRouter } from "./routes/user.router";
 
+const origins = process.env.NODE_ENV == "development" ? 
+  [ "http://localhost:5173", "http://localhost:8000" ] : 
+  [];
 const app: Application = express();
+
+app.set("trust proxy", true);
+
+app.use(cors({ origin: origins, credentials: true }));
+app.use(helmet({ 
+  crossOriginResourcePolicy: process.env.NODE_ENV != "development", 
+  contentSecurityPolicy: false,
+  crossOriginOpenerPolicy: false,
+}));
 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
